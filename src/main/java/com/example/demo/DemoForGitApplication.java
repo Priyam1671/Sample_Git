c0fd67e453536edad193fab58adbb8eafd1c2b83
Priyam <spriyam522@gmial.com>
2025-06-29 23:39:22 +0530

    second commit

--- src/main/java/com/example/demo/DemoForGitApplication.java
+++ src/main/java/com/example/demo/DemoForGitApplication.java
@@ -8,7 +8,7 @@ public class DemoForGitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoForGitApplication.class, args);
-		System.out.println("Added by Lead");
+		System.out.println("Added by Lead in the master");
 	}
 
 }
